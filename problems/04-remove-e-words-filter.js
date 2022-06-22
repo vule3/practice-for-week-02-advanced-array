038c006b83c60fe9b2504690cd439fca02ce0d0e
vule <vule2042@gmail.com>
2022-06-21 21:02:37 -0500

    Update 04-remove-e-words-filter.js

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -12,8 +12,17 @@ console.log(removeEWords('Enter the building')); // 'building'
 
 */
 
-let removeEWords = function(sentence) {
-    // Your code here
+let removeEWords = function (sentence) {
+  let arr = sentence.split(" ")
+  let newArr = [];
+
+  let removed = arr.filter(function (chars) {
+    if (!chars.includes("e")) {
+      newArr.push(chars);
+    }
+  });
+
+  return newArr.join(" ");
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -22,4 +31,4 @@ try {
   module.exports = removeEWords;
 } catch (e) {
   module.exports = null;
-}
+}
