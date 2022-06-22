bda363df752c70a85491f2a0a484ae5abfa8f744
vule <vule2042@gmail.com>
2022-06-21 20:49:37 -0500

    Update 03-long-words-filter.js

--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -13,8 +13,9 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 
 */
 
-let longWords = function(words) {
-    // Your code here
+let longWords = function (words) {
+  let filtered = words.filter((chars) => chars.length > 5);
+  return filtered;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -23,4 +24,4 @@ try {
   module.exports = longWords;
 } catch (e) {
   module.exports = null;
-}
+}
