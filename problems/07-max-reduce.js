767be8f3b0a1cc6f1820b839f47c1074ff005992
vule <vule2042@gmail.com>
2022-06-21 21:37:38 -0500

    Update 07-max-reduce.js

--- problems/07-max-reduce.js
+++ problems/07-max-reduce.js
@@ -14,8 +14,14 @@ console.log(maxWithReduce([-2, -3, -7, 3])); // 3
 
 */
 
-let maxWithReduce = function(nums) {
-    // Your code here
+let maxWithReduce = function (nums) {
+  return nums.reduce(function (current, next) {
+    if (current > next) {
+      return current;
+    } else {
+      return next;
+    }
+  });
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
